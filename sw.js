6fb5db33f6b420c275c7890611db2c3b828dd433
Matti4smart <m.gorecki@g.pl>
2026-09-25 09:07:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const V='ludzie-v2';
-const SHELL=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png',
+const SHELL=['./','index.html','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png',
  'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.2/cropper.min.css','https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.2/cropper.min.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==V).map(x=>caches.delete(x)))));self.clients.claim()});
